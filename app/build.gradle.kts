6ffa6102f4d0d85591af153e3ea441f3701d3d63
travellerse <travellers0238@outlook.com>
2025-11-14 17:13:43 +0800

    ci: 添加.pre-commit配置文件以支持代码检查和格式化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ktlint)
 }
 
 android {
@@ -57,4 +58,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+
+ktlint {
+    version.set(libs.versions.ktlintCli.get())
+    android.set(true)
 }
